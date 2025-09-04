df6728d9315aaf0f76c35b20be671accd09bf0a7
VedPrakash-cloud <rocky007ved@gmail.com>
2025-09-05 00:09:58 +0530

    button type commit

--- src/component/displayName.jsx
+++ src/component/displayName.jsx
@@ -31,7 +31,7 @@ export default function DisplayName() {
           required
         />
         <br />
-        <button value="submit">
+        <button type="submit">
           Submit
         </button>
         </form>
